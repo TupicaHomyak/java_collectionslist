c9d25acae43f9491020b4d4f18b1cb4c4084acb5
Olga Malkova <Vodeviiil@mail.ru>
2023-05-16 14:17:28 +0300

    практ.задание 10.9

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,43 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите команду (LIST, ADD, EDIT, DELETE, EXIT):");
+            String command = scanner.nextLine();
+
+            if (command.equalsIgnoreCase("LIST")) {
+                todoList.getTodos();
+            } else if (command.equalsIgnoreCase("ADD")) {
+                System.out.println("Введите дело для добавления:");
+                String todo = scanner.nextLine();
+                todoList.add(todo);
+            } else if (command.equalsIgnoreCase("EDIT")) {
+                System.out.println("Введите номер дела для редактирования:");
+                int index = scanner.nextInt();
+                scanner.nextLine(); // Считываем символ новой строки
+                System.out.println("Введите новое дело:");
+                String todo = scanner.nextLine();
+                todoList.edit(index, todo);
+            } else if (command.equalsIgnoreCase("DELETE")) {
+                System.out.println("Введите номер дела для удаления:");
+                int index = scanner.nextInt();
+                scanner.nextLine(); // Считываем символ новой строки
+                todoList.delete(index);
+            } else if (command.equalsIgnoreCase("EXIT")) {
+                System.out.println("Программа завершена.");
+                break;
+            } else {
+                System.out.println("Некорректная команда. Попробуйте ещё раз.");
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -1,31 +1,67 @@
 package practice;
-
 import java.util.ArrayList;
 
 public class TodoList {
+    private ArrayList<String> todos;
+
+    public TodoList() {
+        todos = new ArrayList<>();
+    }
 
     public void add(String todo) {
+        todos.add(todo);
+        System.out.println("Дело добавлено.");
         // TODO: добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
+        if (index >= 0 && index <= todos.size()) {
+            todos.add(index, todo);
+            System.out.println("Дело добавлено на указанный индекс.");
+        } else {
+            todos.add(todo); // Добавляем дело в конец списка
+            System.out.println("Некорректный индекс. Дело добавлено в конец списка.");
+        }
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
     }
 
     public void edit(int index, String todo) {
+        if (index >= 0 && index < todos.size()) {
+            todos.set(index, todo);
+            System.out.println("Дело отредактировано.");
+        } else {
+            System.out.println("Некорректный индекс. Дело не отредактировано.");
+        }
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
     }
 
     public void delete(int index) {
+        if (index >= 0 && index < todos.size()) {
+            todos.remove(index);
+            System.out.println("Дело удалено.");
+        } else {
+            System.out.println("Некорректный индекс. Дело не удалено.");
+        }
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
     }
 
-    public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
-    }
 
+    public ArrayList<String> getTodos() {
+        if (todos.isEmpty()) {
+            System.out.println("Список дел пуст.");
+        } else {
+            System.out.println("Список дел:");
+            for (int i = 0; i < todos.size(); i++) {
+                System.out.println((i + 1) + ". " + todos.get(i));
+            }
+        }
+        return todos;
+        // TODO: вернуть список дел
+    }
 }
+
+
+
